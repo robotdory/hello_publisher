2f226a266690bbcf780dc1d8794f093b54e21e64
TAEHYEON KIM <kimth@robotis.com>
2024-03-22 13:50:34 +0900

    change publisher node
    Signed-off-by: TAEHYEON KIM <kimth@robotis.com>

--- include/hello_publisher/hello_publisher.hpp
+++ include/hello_publisher/hello_publisher.hpp
@@ -1,18 +1,18 @@
 /*******************************************************************************
-* Copyright 2024 ROBOTIS CO., LTD.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright 2024 ROBOTIS CO., LTD.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 /* Authors: Tae-Hyeon KIM */
 
 #ifndef HELLO_PUBLISHER__HELLO_PUBLISHER_HPP_
@@ -22,24 +22,26 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "std_msgs/msg/string.hpp"
-#include "std_msgs/msg/bool.hpp"
 
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/string.hpp"
 
-namespace hello_publisher
-{
-class HelloPublisher : public rclcpp::Node
-{
-public:
-  explicit HelloPublisher(const std::string & node_name);
+namespace hello_publisher {
+class HelloPublisher : public rclcpp::Node {
+ public:
+  explicit HelloPublisher(const std::string& node_name);
 
   virtual ~HelloPublisher();
 
-private:
+ private:
   rclcpp::TimerBase::SharedPtr publish_info_timer_;
+  rclcpp::TimerBase::SharedPtr publish_heartbeat_timer_;
+
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr hello_publisher_;
   rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr heartbeat_publisher_;
+
+  size_t message_count_;
   /**
    * @brief Topic과 Heartbeat를 Publish하는 함수
    *
